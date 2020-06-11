2972261a507d1c37795b62f60ece816dad5e5aac
Ulza-0 <63139346+Ulza-0@users.noreply.github.com>
2020-06-11 23:16:35 +0500

    9 лаба

---
+++ udolguntseva/Lab 9/9/9.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    std::cout << "Vvedite stroku (iz neskolkih slov): ";
+    std::string s;
+    std::getline(std::cin, s);
+    string l= s.substr(s.find_last_of(' '));
+    int n = 0;
+    for (int m = 0; m < l.length(); m++)
+    {
+        if (l[m] == 'k')
+        {
+            n++;
+        }
+    }
+    cout << "resultat: "
+        << n, "\n";
+    return 0;
+}
