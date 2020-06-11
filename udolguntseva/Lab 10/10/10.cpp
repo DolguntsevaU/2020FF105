1ddcc7529721eabd9c678a8280b316ff07d1333b
Ulza-0 <63139346+Ulza-0@users.noreply.github.com>
2020-06-12 00:39:16 +0500

    Lab 10

---
+++ udolguntseva/Lab 10/10/10.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c = 0.0;
+	cout << "Введите делимое: ";
+	cin >> a;
+	cout << "Введите делитель: ";
+	cin >> b;
+	if (b == 0)
+	{
+		cout << "На 0 делить нельзя!" << endl;
+		return 1;
+	}
+	c = a / b;
+	cout << "Частное равно: " << c << endl;
+	return 0;
+}
+
+
