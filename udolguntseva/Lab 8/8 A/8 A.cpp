ceee7e791c5b034432ba7299456ded4058b86106
Ulza-0 <63139346+Ulza-0@users.noreply.github.com>
2020-06-11 22:15:34 +0500

    8 Лаба

---
+++ udolguntseva/Lab 8/8 A/8 A.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int i, j, n, m;
+    cout << "Введите параметр матрицы: ","\n";
+    cin >> n;
+    int** M = new int* [n];
+    for (i = 0; i < n; i++)
+        M[i] = new int[n];
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            M[i][j] = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        m = i+1;
+        for (j = 0; j < n;  j++)
+        {
+            if (m <= n)
+            {
+                M[i][j] = (m) % (n + 1);
+                m++;
+            }
+            
+        }
+        
+    }
+            
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+            cout << M[i][j] << ' ';
+        cout << endl;
+    }
+    for (i = 0; i < n; i++)
+        delete[] M[i];
+    delete[] M;
+    return 0;
+}
+
+
---
+++ udolguntseva/Lab 8/8 B/8 B.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int П, Т;
+    cout << "Введите размеры матрицы:" << endl;
+    cout << "П = ";
+    cin >> П;
+    cout << "Т = ";
+    cin >> Т;
+    int** A = new int* [П];
+    for (int i = 0; i < П; i++)
+    {
+        A[i] = new int[Т];
+    }
+    cout << "Введите матрицу A[П][Т]:" << endl;
+    for (int i = 0; i < П; i++)
+    {
+        for (int j = 0; j < Т; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+    cout << "Упорядоченная матрица A[П][Т]:" << endl;
+    for (int i = 0; i < П; i++)
+    {
+        for (int j = 0; j < Т; j++)
+        {
+            sort(A[i], A[i] + Т);
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < П; i++)
+    {
+        delete[] A[i];
+    }
+    
+    return 0;
+}
+
+
